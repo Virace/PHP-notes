010f05d7004b02097ec3306aa9ab6db2f1442310
Virace <woaibzt@vip.qq.com>
2016-09-29 13:46:23 +0800

    2016年9月29日 13:46:21

--- Var/varBoolean.php
+++ Var/varBoolean.php
@@ -1,7 +1,15 @@
 <<?php
 /**
- *
+ * boolean布尔型
+ * @var boolean
+ * false=非零、空字符串(空格为真)、一个0的字符串、空数组、没有对象的Object(此条为PHP4实用)、NULL
  */
- 
+          $bool=100;
+          if($bool){
+                    echo "真";
+          }else{
+                    echo "假";
+          }
+
 
  ?>
