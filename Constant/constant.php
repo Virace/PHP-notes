af40a85f33f41ec4bc92e47bf35d24a4fc432a51
Virace <woaibzt@vip.qq.com>
2016-09-30 17:36:26 +0800

    2016年9月30日 17:36:25

---
+++ Constant/constant.php
@@ -0,0 +1,14 @@
+<?php
+          error_reporting( E_ALL & ~E_NOTICE);
+    //Notice: Use of undefined constant ROOT - assumed 'ROOT'
+/**
+ * 常量：一单声明，这个常量的值就不会改变
+ * 1.常量似乎用函数define()声明的
+ * 2.如果常量没有声明，则常量名在使用时，会自动转为字符串 (与正常字符串生成速度慢8倍)
+ */
+          define("ROOT", "localhost");
+          echo ROOT;
+
+
+
+ ?>
