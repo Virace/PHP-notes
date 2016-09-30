68856723482f975f8f3c4c1392db6fee1af12f5a
Virace <woaibzt@vip.qq.com>
2016-09-30 17:48:40 +0800

    2016年9月30日 17:48:37

---
+++ Constant/constantDec.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 常量名称不能加$符号
+ * 常量名称，默认是区分大小写的，习惯上常量全部大写
+ * 可以使用 define($constant_name, $value, $case_insensitive) 第三个参数决定是否区分大小写(逻辑)
+ * 常量的值只支持标量数据类型
+ * 常量不能使用unset() 清除常量！
+ * 可使用defined(),判断一个常量是否存在
+ * get_defined_constants()获取当前所有已定义的常量
+ */
+
+
+ ?>
