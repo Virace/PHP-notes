a23d3f3666b93d0dfd0c0774c99558b51913c137
Virace <woaibzt@vip.qq.com>
2016-10-02 12:57:48 +0800

    2016年10月2日 12:57:46

--- Operator/ohterOperators.php
+++ Operator/ohterOperators.php
@@ -3,7 +3,11 @@
 /**
  * 问号表达式
  * 反引号 `系统命令` 执行系统命令返回(安全问题、不跨平台)
+ * @ 错误控制(错误屏蔽)
  */
 
-          echo `shutdown -r`;
+//          echo `ipconfig`;
+          echo @$a;
  ?>
+<br>
+<img src="./img/2.jpg" alt="" />
